5cb1a006fd89077522d4ddafccb2a997f3171292
anna <slukin1@yandex.ru>
2022-08-20 15:34:19 +0700

    tesk36

---
+++ Task36/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: 
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    if (size == 0) return new int[] { };
+
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int SumDigit(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+int[] arr = CreateArrayRndInt(10, -7, 7);
+int sumDigit = SumDigit(arr);
+PrintArray(arr);
+Console.Write($" -> {sumDigit}");
